588c1428cbbde4ffabf894ff5e4ee544ee5a8717
Srinivasa Rao Gurram <srinivasaraogurram@users.noreply.github.com>
2024-06-12 03:49:10 +0000

    adding python from w3schools

---
+++ python-w3schools/print-values.py
@@ -0,0 +1,17 @@
+# Output Variables
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
---
+++ python-w3schools/variables.py
@@ -0,0 +1,52 @@
+x = 5
+y = "Hello, World!"
+print(x, y)
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+# Variable names are case-sensitive.
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+"""
+Variable Names
+A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+"""
+#Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Illegal variable names:
+"""
+2myvar = "John"
+my-var = "John"
+my var = "John"
+"""
+# Many Values to Multiple Variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+#One Value to Multiple Variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
